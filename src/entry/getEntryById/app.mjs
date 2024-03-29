75f47c8add7e987dd3354548acc55269a02773a9
nguyenngocpfievk56 <nguyen.huu.ngoc@elife.co.jp>
2024-03-29 14:43:09 +0700

    update

--- src/entry/getEntryById/app.mjs
+++ src/entry/getEntryById/app.mjs
@@ -1,7 +1,7 @@
 export const lambdaHandler = async (event, _) => {
     const data = {
         id: 1,
-        title: 'PROD1 1 version 3',
+        title: 'PROD1 1 for cs2p_ngocnh',
         content: 'This is the first PROD1 entry',
     };
 
